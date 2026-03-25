11e8a51b6c9d2f673995d3d48983e9ce120150ca
Varun Dev Mittal <varunm55@gmail.com>
2026-03-25 18:18:00 +0530

    puh

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import YTClientsPage from './pages/YTClientsPage.jsx'
+
+const page = new URLSearchParams(window.location.search).get('page')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {page === 'yt-clients' ? <YTClientsPage /> : <App />}
   </StrictMode>,
 )
